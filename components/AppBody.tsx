cef9d9364162e17ee04d73c38709b8c2915a5f68
Grivj <jgriveau@yahoo.fr>
2021-11-20 11:29:06 +0100

    New font + minor styling

--- components/AppBody.tsx
+++ components/AppBody.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react"
 import Code from "./AppCode"
 interface Props {
     tabIndex: number,
@@ -6,30 +6,42 @@ interface Props {
 
 const TabContentSkills = () => (
     <Flex flexDir="column">
-        <Code line={1} indent={0} code="deep_learning = {" type="python"/>
-        <Code line={2} indent={1} code='"Python": "~ 2 years",' type="python"/>
-        <Code line={3} indent={1} code='"Pytorch": "~ 2 years",' type="python"/>
-        <Code line={4} indent={0} code="}" type="python"/>
-        <Code line={5} indent={0} code="" type="python"/>
-        <Code line={6} indent={0} code="web_development = {" type="python"/>
-        <Code line={7} indent={1} code='"React": "< 1 year"' type="python"/>
-        <Code line={8} indent={1} code='"Next.js": "< 1 year"' type="python"/>
-        <Code line={9} indent={1} code='"PHP": "~ 5 year"' type="python"/>
-        <Code line={10} indent={1} code='"HTML": "~ 5 year"' type="python"/>
-        <Code line={11} indent={1} code='"CSS": "~ 5 year"' type="python"/>
-        <Code line={12} indent={0} code="}" type="python"/>
-        <Code line={13} indent={0} code="" type="python"/>
+        <Code line={1} indent={0} code="deep_learning = {" type="python" />
+        <Code line={2} indent={1} code='"Python": "~ 2 years",' type="python" />
+        <Code line={3} indent={1} code='"Pytorch": "~ 2 years",' type="python" />
+        <Code line={4} indent={0} code="}" type="python" />
+        <Code line={5} indent={0} code="" type="python" />
+        <Code line={6} indent={0} code="web_development = {" type="python" />
+        <Code line={7} indent={1} code='"React": "< 1 year"' type="python" />
+        <Code line={8} indent={1} code='"Next.js": "< 1 year"' type="python" />
+        <Code line={9} indent={1} code='"PHP": "~ 5 year"' type="python" />
+        <Code line={10} indent={1} code='"HTML": "~ 5 year"' type="python" />
+        <Code line={11} indent={1} code='"CSS": "~ 5 year"' type="python" />
+        <Code line={12} indent={0} code="}" type="python" />
+        <Code line={13} indent={0} code="" type="python" />
+    </Flex>
+)
+
+const TabContentInterests = () => (
+    <Flex flexDir="column">
+        <Code line={1} indent={0} code="{" type="json" />
+        <Code line={2} indent={1} code='"include": [' type="json" />
+        <Code line={3} indent={2} code='"Something, Something",' type="json" />
+        <Code line={4} indent={2} code='"Something else",' type="json" />
+        <Code line={5} indent={1} code="]" type="json" />
+        <Code line={6} indent={0} code="}" type="json" />
     </Flex>
 )
 
 
 const AppBody = (props: Props) => {
 
-
     const contentSwitch = (contentId: number): JSX.Element => {
         switch (contentId) {
             case 0:
                 return <TabContentSkills />
+            case 1:
+                return <TabContentInterests />
 
             default:
                 return <>Content not found</>;
--- components/AppCode.tsx
+++ components/AppCode.tsx
@@ -16,15 +16,30 @@ type CodeTextProps = {
 }
 
 const CodeText = ({ text, type, indent }: CodeTextProps) => (
-    <Box as="span" style={{ textIndent: indent * 45 }} display="inline-block">
+    <Box as="span"
+        style={{ textIndent: indent * 45 }}
+        display="inline-block"
+        fontFamily="Cascadia Code"
+    >
         {text}
     </Box>
 )
 
 const Code = (props: CodeProps) => (
     <Box>
-        <Box as="span" mr="40px" w="20px" display="inline-block" textAlign="right">{props.line}</Box>
+        <Box as="span"
+            mr="40px"
+            w="20px"
+            display="inline-block"
+            textAlign="right"
+            color="#858585"
+            fontFamily="Cascadia Code"
+        >
+            {props.line}
+        </Box>
+
         <CodeText text={props.code} type={props.type} indent={props.indent} />
+        {console.log(props.code.split(/[^a-z_.1-9<>~]/i))}
     </Box>
 )
 
