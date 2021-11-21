b63ee22085fee645ba590f74587c046235b3fb5b
Grivj <jgriveau@yahoo.fr>
2021-11-21 11:53:24 +0100

    Tab content education + base experience

--- components/AppBody.tsx
+++ components/AppBody.tsx
@@ -33,6 +33,39 @@ const TabContentInterests = () => (
     </Flex>
 )
 
+const TabContentEducation = () => (
+    <Flex flexDir="column">
+        <Code line={1} indent={0} code="# MS Computer Science" type="md" />
+        <Code line={2} indent={0} code='## Hunan University - China, Changsha' type="md" />
+        <Code line={3} indent={1} code='> Research on the detection of adversarial examples' type="md" />
+        <Code line={4} indent={1} code='in deep learning, computer vision.' type="md" />
+        <Code line={5} indent={1} code="* Note: Studying under the CSC Scholarship" type="md" />
+        <Code line={6} indent={0} code="" type="" />
+
+        <Code line={7} indent={0} code="# Mandarin language courses" type="md" />
+        <Code line={8} indent={0} code='## Hunan University - China, Changsha' type="md" />
+        <Code line={9} indent={1} code='> Studying Mandarin while exploring Hunan, China.' type="md" />
+        <Code line={10} indent={1} code="* Note: Obtained the HSK3 certification" type="md" />
+        <Code line={11} indent={0} code="" type="" />
+
+        <Code line={12} indent={0} code="# Bachelor digital project manager" type="md" />
+        <Code line={13} indent={0} code='## IESA - France, Paris' type="md" />
+        <Code line={14} indent={1} code="* Note: Certified title level II (FR) 6 (EU) (NSF320p-326p)" type="md" />
+        <Code line={15} indent={0} code="" type="" />
+    </Flex>
+)
+
+const TabContentExperience = () => (
+    <Flex flexDir="column">
+        <Code line={1} indent={0} code="{" type="json" />
+        <Code line={2} indent={1} code='"include": [' type="json" />
+        <Code line={3} indent={2} code='"Something, Something",' type="json" />
+        <Code line={4} indent={2} code='"Something else",' type="json" />
+        <Code line={5} indent={1} code="]" type="json" />
+        <Code line={6} indent={0} code="}" type="json" />
+    </Flex>
+)
+
 
 const AppBody = (props: Props) => {
 
@@ -42,6 +75,10 @@ const AppBody = (props: Props) => {
                 return <TabContentSkills />
             case 1:
                 return <TabContentInterests />
+            case 2:
+                return <TabContentEducation />
+            case 3:
+                return <TabContentExperience />
 
             default:
                 return <>Content not found</>;
