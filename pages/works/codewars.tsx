e2fa02db83a95c30184b486d0d3031a543bc33a5
Grivj <jgriveau@yahoo.fr>
2021-12-03 11:59:25 +0100

    Some line breaks for "better" readability in the code snippets

--- pages/works/codewars.tsx
+++ pages/works/codewars.tsx
@@ -4,8 +4,12 @@ import Work, { WorkBody, WorkHeader, WorkSectionTitle } from "../../components/A
 
 const sudokuCode = `
 """
-Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
-The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
+Write a function that will solve a 9x9 Sudoku puzzle.
+The function will take one argument consisting of the 2D puzzle array,
+with the value 0 representing an unknown square.
+The Sudokus tested against your function will be "easy"
+(i.e. determinable; there will be no need to assume and test possibilities on unknowns)
+and can be solved with a brute-force approach.
 """
 
 import time
@@ -151,7 +155,10 @@ if __name__ == '__main__':
 
 const observedCode = `
 """
-    Alright, detective, one of our colleagues successfully observed our target person, Robby the robber. We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure about the PIN he saw, when Robby entered it.
+    Alright, detective, one of our colleagues successfully observed our target person, Robby the robber.
+    We followed him to a secret warehouse, where we assume to find all the stolen stuff.
+    The door to this warehouse is secured by an electronic combination lock.
+    Unfortunately our spy isn't sure about the PIN he saw, when Robby entered it.
     The keypad has the following layout:
     ┌───┬───┬───┐
     │ 1 │ 2 │ 3 │
@@ -162,13 +169,23 @@ const observedCode = `
     └───┼───┼───┘
         │ 0 │
         └───┘
-    He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
-    He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they never finally lock the system or sound the alarm. That's why we can try out all possible (*) variations.
+    He noted the PIN 1357, but he also said,
+    it is possible that each of the digits he saw could actually be another adjacent digit
+    (horizontally or vertically, but not diagonally).
+    E.g. instead of the 1 it could also be the 2 or 4.
+    And instead of the 5 it could also be the 2, 4, 6 or 8.
+    He also mentioned, he knows this kind of locks.
+    You can enter an unlimited amount of wrong PINs, they never finally lock the system or sound the alarm.
+    That's why we can try out all possible (*) variations.
     * possible in sense of: the observed PIN itself and all variations considering the adjacent digits
-    Can you help us to find all those variations? It would be nice to have a function, that returns an array (or a list in Java and C#) of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs (get_pins in python, GetPINs in C#). But please note that all PINs, the observed one and also the results, must be strings, because of potentially leading '0's. We already prepared some test cases for you.
+    Can you help us to find all those variations?
+    It would be nice to have a function,
+    that returns an array of all variations for an observed PIN with a length of 1 to 8 digits.
+    We could name the function getPINs (get_pins in python, GetPINs in C#).
+    But please note that all PINs, the observed one and also the results, must be strings,
+    because of potentially leading '0's. We already prepared some test cases for you.
     Detective, we are counting on you!
 """
-from itertools import product
 
 
 def get_pins(observed):
