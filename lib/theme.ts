2b0b1a8b369204b71cdae228099565c6f9beeeb1
Grivj <jgriveau@yahoo.fr>
2021-12-16 10:30:44 +0100

    Removing background color, iPad safari looks bad

--- lib/theme.ts
+++ lib/theme.ts
@@ -5,7 +5,6 @@ const styles = {
     body: {
       textRendering: "optimizeLegibility",
       WebkitFontSmoothing: "antialiased",
-      backgroundColor: "#4158D0",
       backgroundImage:
         "linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)",
     },
