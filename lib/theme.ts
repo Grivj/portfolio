9321cd09330e4a2de96d497f5b144927ac3bc8de
Jordan Griveau <jgriveau@yahoo.fr>
2021-12-27 13:57:09 +0100

    Update theme.ts: change default chackra font color
    On some navigators the default font color is black, from chackra ui light theme

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,6 +3,7 @@ import { extendTheme } from "@chakra-ui/react";
 const styles = {
   global: {
     body: {
+      color: "white";
       textRendering: "optimizeLegibility",
       WebkitFontSmoothing: "antialiased",
       background:
