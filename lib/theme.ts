64592a46d20fb6407ff2e94688f97c42db9a1105
Jordan Griveau <jgriveau@yahoo.fr>
2021-12-27 13:57:40 +0100

    Update theme.ts
    Fix end of line typo

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,7 +3,7 @@ import { extendTheme } from "@chakra-ui/react";
 const styles = {
   global: {
     body: {
-      color: "white";
+      color: "white",
       textRendering: "optimizeLegibility",
       WebkitFontSmoothing: "antialiased",
       background:
